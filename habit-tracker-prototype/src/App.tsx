89a116b5aa56044f5dcaa87f9bd0bd4b49555ab8
Sami <71572388+varadars@users.noreply.github.com>
2025-05-08 20:49:47 -0700

    Playing with Tailwind
    Changed the css on some components but didn't do much.

--- habit-tracker-prototype/src/App.tsx
+++ habit-tracker-prototype/src/App.tsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
+import type { MouseEvent } from "react";
 import { supabase } from "./lib/client";
 
 function App() {
-  const [groups, setGroups] = useState([]);
+  type Group = {
+    id: string;
+    name: string;
+    description: string;
+  };
+
+  const [groups, setGroups] = useState<Group[]>([]);
 
   useEffect(() => {
     getGroups();
@@ -11,20 +18,48 @@ function App() {
   async function getGroups() {
     const { data, error } = await supabase
       .from("groups")
-      .select();
-    setGroups(data);
+      .select(`id, name, description, players (player_id)`);
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    if (data) {
+      setGroups(data);
+      console.log(data);
+    }
   }
 
+  const handleClick = (
+    event: MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    window.open("https://www.google.com", "_blank");
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="grid min-h-screen place-items-center">
       <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind + Vite!
+        Hello Players!
       </h1>
-      <ul>
-        {groups.map((group) => (
-          <li key={group.name}>{group.name}</li>
-        ))}
-      </ul>
+      <section className="bg-gray-100 shadow rounded-2xl p-6">
+        <ul className="grid gap-5">
+          {groups.map((group) => (
+            <li key={group.name}>
+              <button
+                className="w-full max-w-sm text-left p-4 bg-white rounded-2xl shadow hover:shadow-lg transition hover:bg-gray-200"
+                onClick={handleClick}
+              >
+                <h2 className="text-2xl font-bold mb-4">
+                  {group.name}
+                </h2>
+                <p>{group.description}</p>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
--- habit-tracker-prototype/src/main.tsx
+++ habit-tracker-prototype/src/main.tsx
@@ -1,4 +1,3 @@
-import App from './App.tsx'
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
