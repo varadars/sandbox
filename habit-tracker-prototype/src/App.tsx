33d4c72d8a3003d786011a390a4089185dad92c7
Sami <71572388+varadars@users.noreply.github.com>
2025-05-09 14:32:21 -0700

    added router and fixed some spacing

--- habit-tracker-prototype/src/App.tsx
+++ habit-tracker-prototype/src/App.tsx
@@ -39,16 +39,16 @@ function App() {
   };
 
   return (
-    <div className="grid min-h-screen place-items-center">
-      <h1 className="text-4xl font-bold text-blue-600">
+    <div className="flex flex-col items-center min-h-screen justify-center">
+      <h1 className="text-5xl font-bold text-blue-600 mb-12">
         Hello Players!
       </h1>
       <section className="bg-gray-100 shadow rounded-2xl p-6">
-        <ul className="grid gap-5">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
           {groups.map((group) => (
             <li key={group.name}>
               <button
-                className="w-full max-w-sm text-left p-4 bg-white rounded-2xl shadow hover:shadow-lg transition hover:bg-gray-200"
+                className="w-full max-w-xs h-60 flex flex-col justify-center text-left p-4 bg-white rounded-2xl shadow hover:shadow-lg transition hover:bg-gray-200"
                 onClick={handleClick}
               >
                 <h2 className="text-2xl font-bold mb-4">
