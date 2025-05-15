6d14444d2da0926b32c8a9d8e048cf62615554cd
Sami <71572388+varadars@users.noreply.github.com>
2025-05-14 20:13:21 -0700

    fix error

--- habit-tracker-prototype/src/pages/PlayerDash.tsx
+++ habit-tracker-prototype/src/pages/PlayerDash.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { supabase } from "../lib/client";
 
