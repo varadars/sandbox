35e960700742eb995735061e36e271532b103f48
Sami <71572388+varadars@users.noreply.github.com>
2026-04-14 19:15:53 -0700

    fixed #7
    Apparently you can't access other files on the client side so I just made the projects a bit easier to add and added in some flexbox stuff for now

---
+++ projects.js
@@ -0,0 +1,71 @@
+export const projects = [
+  {
+    id: 0,
+    name: "Video Background",
+    date_created: new Date("04/20/25"),
+    src: "./proj-web-dev/bt-vid4/index.html",
+    description:
+      "Followed a Brad Traversy tutorial focused on having a video background and implementing some css positioning for the menu.",
+    tags: [
+      "video-background",
+      "brad-traversy",
+      "css-focused",
+    ],
+  },
+  {
+    id: 1,
+    name: "Writings for Devs",
+    date_created: new Date("04/24/25"),
+    src: "./proj-web-dev/rm-fs-static-wp/index.html",
+    description:
+      "Recommend by I believe the Frontend Roadmap on roadmap.sh as a milestone project for CSS understanding. Hardest part was the fade out on the right side of the nav.",
+    tags: ["roadmap-sh", "css-focused"],
+  },
+  {
+    id: 2,
+    name: "Visit Pooprisland",
+    date_created: new Date("3/23/26"),
+    src: "./proj-web-dev/scrimba-hometown/index.html",
+    description:
+      "Milestone project from scrimba.com from the introductory HTML/CSS section. Had fun absolutely positioning a transparent picture of my cat as the mayor.",
+    tags: ["cat", "css-focused", "scrimba"],
+  },
+  {
+    id: 4,
+    name: "Password Generator",
+    date_created: new Date("3/25/26"),
+    src: "./proj-web-dev/scrimba-password/index.html",
+    description:
+      "Actually getting into good design although the card style is pretty generic. Learned how to replicate auto-layout through code and still want to add in customizability for the password complexity.",
+    tags: ["scrimba", "dark-mode", "actually-useful"],
+  },
+  {
+    id: 3,
+    name: "Simple Scoreboard",
+    date_created: new Date("3/24/26"),
+    src: "./proj-web-dev/scrimba-scoreboard/index.html",
+    description:
+      "Scrimba milestone project for the free Javascript course, didn't really do much design but added in winner color and new game reset.",
+    tags: ["scrimba", "js-focused"],
+  },
+
+  {
+    id: 5,
+    name: "Idli Ordering App",
+    date_created: new Date("4/12/26"),
+    src: "./proj-web-dev/scrimba-ordering-app/index.html",
+    description:
+      "I loved putting in the receipt style order accumulation piece together and the fonts that I found for this perfectly match the vibe that I was going for.",
+    tags: ["scrimba", "design", "actually-useful"],
+  },
+
+  {
+    id: 6,
+    name: "Image Background",
+    date_created: new Date("1/11/25"),
+    src: "./proj-web-dev/bt-vid2/mywebsite/index.html",
+    description:
+      "Horrendous design but one of my first fully-fledged websites with sections and stuff.",
+    tags: ["brad-traversy", "intro", "lorem-ipsum"],
+  },
+];
